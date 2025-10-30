e60398b64eb71532abc73ba978377dadf02f6ee9
Sorensen31 <g.soren.sen2004@gmail.com>
2025-10-29 22:58:42 -0300

    Criando os arquivos e iniciando election

---
+++ src/cliente/core/VotingCliente.java
@@ -0,0 +1,4 @@
+package cliente.core;
+
+public class VotingCliente {
+}
---
+++ src/common/model/Election.java
@@ -0,0 +1,15 @@
+package common.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Election implements Serializable {
+    private String question;
+    private List<String> options;
+
+
+
+
+
+
+}
